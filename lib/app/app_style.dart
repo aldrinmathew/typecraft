2ffabc49d395bf5d7833818c0554406a672584e9
Aldrin Mathew <admin@aldrinmathew.com>
2022-05-04 09:51:02 +0530

    Added `AppStyle`
    - AppStyle contains all common styles used within the app
    
    - The style here refers to the TextStyle variants

---
+++ lib/app/app_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static TextStyle letter = TextStyle(
+    fontFamily: 'Quicksand',
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+    color: Colors.black.withOpacity(0.6),
+  );
+}
