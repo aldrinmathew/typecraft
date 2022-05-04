5b95f5edbc605f1b2632ce858234e088aad4a14b
Aldrin Mathew <admin@aldrinmathew.com>
2022-05-04 11:13:40 +0530

    Added `Intents` for the keys
    - Intents are a flexible system in flutter to use keyboard shortcuts
    
    - Intents for keys have been added to the app

---
+++ lib/model/intent.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+
+List<Intent> allIntents = [
+  IntentKeyBacktick(),
+  IntentKey1(),
+  IntentKey2(),
+  IntentKey3(),
+  IntentKey4(),
+  IntentKey5(),
+  IntentKey6(),
+  IntentKey7(),
+  IntentKey8(),
+  IntentKey9(),
+  IntentKey0(),
+  IntentKeyMinus(),
+  IntentKeyEqual(),
+  IntentKeyBackspace(),
+  IntentKeyTab(),
+  IntentKeyQ(),
+  IntentKeyW(),
+  IntentKeyE(),
+  IntentKeyR(),
+  IntentKeyT(),
+  IntentKeyY(),
+  IntentKeyU(),
+  IntentKeyI(),
+  IntentKeyO(),
+  IntentKeyP(),
+  IntentKeyBracketLeft(),
+  IntentKeyBracketRight(),
+  IntentKeyCapslock(),
+  IntentKeyA(),
+  IntentKeyS(),
+  IntentKeyD(),
+  IntentKeyF(),
+  IntentKeyG(),
+  IntentKeyH(),
+  IntentKeyJ(),
+  IntentKeyK(),
+  IntentKeyL(),
+  IntentKeySemicolon(),
+  IntentKeyQuote(),
+  IntentKeyBackslash(),
+  IntentKeyEnter(),
+  IntentKeyShiftLeft(),
+  IntentKeyZ(),
+  IntentKeyX(),
+  IntentKeyC(),
+  IntentKeyV(),
+  IntentKeyB(),
+  IntentKeyN(),
+  IntentKeyM(),
+  IntentKeyComma(),
+  IntentKeyFullstop(),
+  IntentKeyForwardSlash(),
+  IntentKeyShiftRight(),
+  IntentKeyControlLeft(),
+  IntentKeyAltLeft(),
+  IntentKeySpace(),
+  IntentKeyAltRight(),
+  IntentKeyControlRight(),
+];
+
+class IntentKeyBacktick extends Intent {}
+
+class IntentKey1 extends Intent {}
+
+class IntentKey2 extends Intent {}
+
+class IntentKey3 extends Intent {}
+
+class IntentKey4 extends Intent {}
+
+class IntentKey5 extends Intent {}
+
+class IntentKey6 extends Intent {}
+
+class IntentKey7 extends Intent {}
+
+class IntentKey8 extends Intent {}
+
+class IntentKey9 extends Intent {}
+
+class IntentKey0 extends Intent {}
+
+class IntentKeyMinus extends Intent {}
+
+class IntentKeyEqual extends Intent {}
+
+class IntentKeyBackspace extends Intent {}
+
+class IntentKeyA extends Intent {}
+
+class IntentKeyB extends Intent {}
+
+class IntentKeyC extends Intent {}
+
+class IntentKeyD extends Intent {}
+
+class IntentKeyF extends Intent {}
+
+class IntentKeyG extends Intent {}
+
+class IntentKeyH extends Intent {}
+
+class IntentKeyJ extends Intent {}
+
+class IntentKeyK extends Intent {}
+
+class IntentKeyL extends Intent {}
+
+class IntentKeyM extends Intent {}
+
+class IntentKeyN extends Intent {}
+
+class IntentKeyQ extends Intent {}
+
+class IntentKeyW extends Intent {}
+
+class IntentKeyE extends Intent {}
+
+class IntentKeyR extends Intent {}
+
+class IntentKeyT extends Intent {}
+
+class IntentKeyY extends Intent {}
+
+class IntentKeyU extends Intent {}
+
+class IntentKeyI extends Intent {}
+
+class IntentKeyO extends Intent {}
+
+class IntentKeyP extends Intent {}
+
+class IntentKeyBracketLeft extends Intent {}
+
+class IntentKeyBracketRight extends Intent {}
+
+class IntentKeyS extends Intent {}
+
+class IntentKeyV extends Intent {}
+
+class IntentKeyX extends Intent {}
+
+class IntentKeyZ extends Intent {}
+
+class IntentKeyTab extends Intent {}
+
+class IntentKeyCapslock extends Intent {}
+
+class IntentKeySemicolon extends Intent {}
+
+class IntentKeyQuote extends Intent {}
+
+class IntentKeyBackslash extends Intent {}
+
+class IntentKeyEnter extends Intent {}
+
+class IntentKeyShiftLeft extends Intent {}
+
+class IntentKeyComma extends Intent {}
+
+class IntentKeyFullstop extends Intent {}
+
+class IntentKeyForwardSlash extends Intent {}
+
+class IntentKeyShiftRight extends Intent {}
+
+class IntentKeyControlLeft extends Intent {}
+
+class IntentKeySuperLeft extends Intent {}
+
+class IntentKeyAltLeft extends Intent {}
+
+class IntentKeySpace extends Intent {}
+
+class IntentKeyAltRight extends Intent {}
+
+class IntentKeySuperRight extends Intent {}
+
+class IntentKeyControlRight extends Intent {}
