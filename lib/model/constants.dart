2ffe12dee347a79fb7ebe5f583d31c947cead3a0
Aldrin Mathew <admin@aldrinmathew.com>
2022-05-04 09:47:47 +0530

    Added keys for local storage as constants
    - The constants.dart file now contains the keys that are used throughout
      the app for maintaining local storage

---
+++ lib/model/constants.dart
@@ -0,0 +1,11 @@
+// Key representing the number of words entered by the user, in
+// local storage
+const String wordCountKey = 'AAF_TYPECRAFT_WORD_COUNT';
+// Key representing the number of characters entered by the user,
+// in local storage
+const String characterCountKey = 'AAF_TYPECRAFT_CHARACTER_COUNT';
+// Key representing the total time taken by the user so far, in
+// local storage
+const String timeTakenKey = 'AAF_TYPECRAFT_RUNTIME';
+// Key representing the theme mode of the app, in local storage
+const String themeModeKey = 'AAF_TYPECRAFT_THEME_MODE';
