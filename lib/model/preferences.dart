7eb0f9636f1fb5425af9e5cc655f37f5a1776fa5
Aldrin Mathew <admin@aldrinmathew.com>
2022-05-04 13:44:52 +0530

    Added `Preferences`
    - Instead of using and getting SharedPreferences instances each and
      every time and instead of repeatedly calling functions to manage the
      local storage, now all data related to local storage is maintained by
      the Preferences class
    
    - This updates the local storage with the help of setters

---
+++ lib/model/preferences.dart
@@ -0,0 +1,119 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../app/app_color.dart';
+import '../model/constants.dart';
+
+/// Class that handles all preferences/data used by the app
+class Preferences {
+  static SharedPreferences? _preferences;
+
+  /// Gets values from local storage and assigns those to the relevant fields
+  static init() async {
+    _preferences = await SharedPreferences.getInstance();
+
+    // Theme Mode
+
+    var themeValue = _preferences?.getString(Constants.themeModeKey);
+    switch (themeValue) {
+      case null:
+        {
+          _preferences?.setString(Constants.themeModeKey, 'LIGHT');
+          break;
+        }
+      case 'LIGHT':
+        {
+          _themeMode = ThemeSwitchMode.light;
+          break;
+        }
+      case 'DARK':
+        {
+          _themeMode = ThemeSwitchMode.dark;
+          AppColor.switcher(_themeMode);
+          break;
+        }
+    }
+
+    // Character Count
+    var charValue = _preferences?.getInt(Constants.characterCountKey);
+    switch (charValue) {
+      case null:
+        {
+          _preferences?.setInt(Constants.characterCountKey, 0);
+          break;
+        }
+      default:
+        {
+          _charCount = charValue!;
+        }
+    }
+
+    // Word Count
+    var wordValue = _preferences?.getInt(Constants.wordCountKey);
+    switch (wordValue) {
+      case null:
+        {
+          _preferences?.setInt(Constants.wordCountKey, 8);
+          break;
+        }
+      default:
+        {
+          _wordCount = wordValue!;
+        }
+    }
+
+    // Time Taken
+    var timeValue = _preferences?.getDouble(Constants.timeTakenKey);
+    switch (timeValue) {
+      case null:
+        {
+          _preferences?.setDouble(Constants.timeTakenKey, 0.1);
+          break;
+        }
+      default:
+        {
+          _timeTaken = timeValue!;
+        }
+    }
+  }
+
+  static ThemeSwitchMode _themeMode = ThemeSwitchMode.light;
+
+  /// This setter is expected to be invocated after the state of the theme mode
+  /// is changed in the AppColor instance
+  static set themeMode(ThemeSwitchMode value) {
+    _preferences?.setString(
+        Constants.themeModeKey,
+        (value == ThemeSwitchMode.light)
+            ? 'LIGHT'
+            : (value == ThemeSwitchMode.dark ? 'DARK' : (AppColor.isDarkMode ? 'DARK' : 'LIGHT')));
+    _themeMode =
+        value != ThemeSwitchMode.change ? value : (AppColor.isDarkMode ? ThemeSwitchMode.dark : ThemeSwitchMode.light);
+  }
+
+  static int _charCount = 0;
+
+  static int get charCount => _charCount;
+
+  static set charCount(int value) {
+    _preferences?.setInt(Constants.characterCountKey, value);
+    _charCount = value;
+  }
+
+  static int _wordCount = 8;
+
+  static int get wordCount => _wordCount;
+
+  static set wordCount(int value) {
+    _preferences?.setInt(Constants.wordCountKey, value);
+    _wordCount = value;
+  }
+
+  static double _timeTaken = 0.1;
+
+  static double get timeTaken => _timeTaken;
+
+  static set timeTaken(double value) {
+    _preferences?.setDouble(Constants.timeTakenKey, value);
+    _timeTaken = value;
+  }
+}
