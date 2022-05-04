f3cd28433abc7f1ec6654e675cf7ad894dc71ee0
Aldrin Mathew <admin@aldrinmathew.com>
2022-05-04 11:24:48 +0530

    Changed the string keys to constants
    - The string keys used for local storage has been changed to refer to
      the constant values

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:typecraft/app/app_color.dart';
 
+import 'model/constants.dart';
 import 'screens/home_screen.dart';
 
 void main() async {
   SharedPreferences preferences = await SharedPreferences.getInstance();
   AppColor.preferences = preferences;
-  String? themeMode = preferences.getString('AAF_TYPECRAFT_THEME_MODE');
+  String? themeMode = preferences.getString(themeModeKey);
   if (themeMode == null) {
-    await preferences.setString('AAF_TYPECRAFT_THEME_MODE', 'LIGHT');
+    await preferences.setString(themeModeKey, 'LIGHT');
   } else if (themeMode == 'DARK') {
     AppColor.switcher(ThemeSwitchMode.dark);
   }
