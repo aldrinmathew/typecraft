d09b2865ead6bf98826cb5a6f8636922ab6add35
Aldrin Mathew <admin@aldrinmathew.com>
2024-04-13 16:16:51 +0530

    New Build - 2024-04-13 16:16:49

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-git push origin -d gh-pages && git subtree push --prefix build/web origin gh-pages
+printf -v flutter_timestamp '%(%Y-%m-%d %H:%M:%S)T' -1 \
+	&& flutter build web --release --base-href=/typecraft/ \
+	&& git add . \
+	&& git commit -m "New Build - ${flutter_timestamp}" \
+	&& git push origin -d gh-pages \
+	&& git subtree push --prefix build/web origin gh-pages
